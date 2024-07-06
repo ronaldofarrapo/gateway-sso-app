60f7366866a5b914647fc13ffcfa5817080fbb65
ronaldo.farrapo <ronaldo.farrapo@mprj.mp.br>
2024-07-06 17:07:56 -0300

    feat: initial commit

---
+++ src/main/java/com/github/ronaldofarrapo/gateway_sso_app/GatewaySsoAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.ronaldofarrapo.gateway_sso_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySsoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySsoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ronaldofarrapo/gateway_sso_app/GatewaySsoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ronaldofarrapo.gateway_sso_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySsoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
